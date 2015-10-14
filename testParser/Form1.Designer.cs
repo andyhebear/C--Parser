43249d7cf208bb24a4cac2ecbf57988d99084b18
RainsSoft <andyhebear@163.com>
2015-10-14 16:22:24 +0800

    add a test

---
+++ testParser/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace testParser {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.button_open = new System.Windows.Forms.Button();
+            this.button_Parser = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox_csfile = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(350, 61);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(75, 23);
+            this.button_open.TabIndex = 0;
+            this.button_open.Text = "打开";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // button_Parser
+            // 
+            this.button_Parser.Location = new System.Drawing.Point(431, 61);
+            this.button_Parser.Name = "button_Parser";
+            this.button_Parser.Size = new System.Drawing.Size(75, 23);
+            this.button_Parser.TabIndex = 0;
+            this.button_Parser.Text = "执行";
+            this.button_Parser.UseVisualStyleBackColor = true;
+            this.button_Parser.Click += new System.EventHandler(this.button_Execute_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 97);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(568, 369);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox_csfile
+            // 
+            this.comboBox_csfile.FormattingEnabled = true;
+            this.comboBox_csfile.Location = new System.Drawing.Point(33, 35);
+            this.comboBox_csfile.Name = "comboBox_csfile";
+            this.comboBox_csfile.Size = new System.Drawing.Size(568, 20);
+            this.comboBox_csfile.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "执行";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Execute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 502);
+            this.Controls.Add(this.comboBox_csfile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_Parser);
+            this.Controls.Add(this.button_open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Button button_Parser;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox_csfile;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ testParser/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using DDW;
+
+namespace testParser {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+            DirectoryInfo di = new System.IO.DirectoryInfo("tests");
+            if (di.Exists) {
+                FileInfo[] files = di.GetFiles("*.cs", SearchOption.AllDirectories);
+                foreach (var v in files) {
+                    this.comboBox_csfile.Items.Add(v.FullName.Remove(0, di.Parent.FullName.Length+1));
+                }
+            }
+        }
+        private void button_open_Click(object sender, EventArgs e) {
+            OpenFileDialog of = new OpenFileDialog();
+            of.CheckFileExists = true;
+            of.CheckPathExists = true;
+            of.DefaultExt = "*.cs";
+            of.Filter = "*.cs|*.cs";
+            of.Multiselect = false;
+            of.RestoreDirectory = true;
+
+            if (of.ShowDialog() == DialogResult.OK) {
+                this.comboBox_csfile.Text = of.FileName;
+            }
+        }
+        //
+        private void button_Execute_Click(object sender, EventArgs e) {
+           // ParserRunner pr = new ParserRunner();
+            List<Parser.Error>errors=new List<Parser.Error>();
+            CompilationUnitNode cn=ParserRunner.ParseFile(this.comboBox_csfile.Text,out errors);
+            //
+            this.textBox1.Text = "";
+            if (errors.Count > 0) {
+                StringBuilder sb = new StringBuilder();
+                foreach (var v in errors) {
+                    sb.AppendLine(string.Format("({0},{1} token:{2})  {3}",v.Line,v.Column,v.Token,v.Message));
+                }
+                this.textBox1.Text = sb.ToString();
+            }
+            else {
+                StringBuilder sb = new StringBuilder();
+                cn.ToSource(sb);
+                this.textBox1.Text = sb.ToString();
+            }
+
+        }
+
+
+
+    }
+}
